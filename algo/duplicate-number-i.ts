bf448f975f4a856984ad1838d4f888924f65dd85
k.greesuradej <world1955@hotmail.com>
2022-07-21 22:43:51 +0700

    did stuff

---
+++ algo/duplicate-number-i.ts
@@ -0,0 +1,13 @@
+function containsDuplicate(nums: number[]): boolean {
+    const set = new Set<number>();
+    
+    for (let i = 0; i < nums.length; i++){
+        if (set.has(nums[i])){
+            return true;
+        }
+        
+        set.add(nums[i]);
+    }
+    
+    return false;
+};
---
+++ algo/duplicate-number-ii.ts
@@ -0,0 +1,33 @@
+function containsNearbyDuplicate(nums: number[], k: number) {
+  const arr: number[] = [];
+  const abs = Math.abs;
+
+  for (let i = 0, length = nums.length; i < length; i++) {
+    if (arr[nums[i]] != undefined) {
+      const j = arr[nums[i]];
+
+      if (abs(i - j) <= k) return true;
+    }
+
+    arr[nums[i]] = i;
+  }
+
+  return false;
+}
+
+// dafuq?
+/**
+ * function containsNearbyDuplicate(nums: number[], k: number): boolean {
+    if (!k) return false;
+    const set = new Set<number>();
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) {
+            return true;
+        } else {
+            if (set.size === k) set.delete(nums[i - k]);
+            set.add(nums[i]);
+        }
+    }
+    return false;
+};
+ */
