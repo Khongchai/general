13c0fb1c84179fd555aea3052889714627e28b85
k.greesuradej <world1955@hotmail.com>
2022-07-24 23:29:37 +0700

    added some fun exercises

---
+++ algo/sliding-window/with-generator.js
@@ -0,0 +1,1 @@
+// Read the comment in with-stream.dart
---
+++ algo/sliding-window/with-stream.dart
@@ -0,0 +1,4 @@
+// Given an array of numbers, return a stream that emits the sum of the numbers in the current sliding window.
+// When calling next, the window should move to the next value and the sum of the updated window should be emitted.
+
+// same goes for with-generator.js
