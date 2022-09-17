de9a0f5776a8966eb488b8977ece368d2b5e1c62
khongchai <world1955@hotmail.com>
2022-09-18 00:30:06 +0700

    kind of works now, actually

--- disambiguation/streams.dart
+++ disambiguation/streams.dart
@@ -1,47 +1,41 @@
 import 'dart:async';
 
 void main() {
-    /// Four main stream classes -> https://dart.academy/streams-and-sinks-in-dart-and-flutter/ 
-    /// Stream: This class represents an asynchronous stream of data. Listeners can subscribe to be notified of the arrival of new data events.
-    /// EventSink: A sink is like a stream that flows in the opposite direction. Adding data events to an EventSink funnels that data into a connected stream.
-    /// StreamController: A StreamController simplifies stream management, automatically creating a stream and sink, and providing methods for controlling a stream's behavior.
-    /// StreamSubscription: Listeners on a stream can save a reference to their subscription, which will allow them to pause, resume, or cancel the flow of data they receive.
+  /// Four main stream classes -> https://dart.academy/streams-and-sinks-in-dart-and-flutter/
+  /// Stream: This class represents an asynchronous stream of data. Listeners can subscribe to be notified of the arrival of new data events.
+  /// EventSink: A sink is like a stream that flows in the opposite direction. Adding data events to an EventSink funnels that data into a connected stream.
+  /// StreamController: A StreamController simplifies stream management, automatically creating a stream and sink, and providing methods for controlling a stream's behavior.
+  /// StreamSubscription: Listeners on a stream can save a reference to their subscription, which will allow them to pause, resume, or cancel the flow of data they receive.
 
-   final streamDisambiguation = StreamDisambiguation(); 
-
-   streamDisambiguation.multiSub();
+  final streamDisambiguation = StreamDisambiguation();
 }
 
 class StreamDisambiguation {
-    /// Listening twice on a single sub will throw 
-    ///  StateError("Stream has already been listened to.")
-    void singleSub() {
-        final controller = StreamController<String>();
+  /// Listening twice on a single sub will throw
+  ///  StateError("Stream has already been listened to.")
+  void singleSub() {
+    final controller = StreamController<String>();
 
-        controller.stream.listen((String data) {
-            print(data);
-        });
-        controller.stream.listen((String data) {
-            print(data);
-        });
+    controller.stream.listen((String data) {
+      print(data);
+    });
+    controller.stream.listen((String data) {
+      print(data);
+    });
 
-        controller.sink.add("Data!");
-    }
+    controller.sink.add("Data!");
+  }
 
-    void multiSub() {
+  void multiSub() {
+    final controller = StreamController<String>.broadcast();
 
-      final controller = StreamController<String>.broadcast();
+    controller.stream.listen((String data) {
+      print(data);
+    });
+    controller.stream.listen((String data) {
+      print(data);
+    });
 
-      controller.stream.listen((String data) {
-          print(data);
-      });
-      controller.stream.listen((String data) {
-          print(data);
-      });
-
-      controller.sink.add("Data!");
-
-    }
-
-
+    controller.sink.add("Data!");
+  }
 }
--- generators/stack_stream_subscription.dart
+++ generators/stack_stream_subscription.dart
@@ -1,73 +1,81 @@
-
 import 'dart:async';
 
-/// An extension to a 
+/// An extension to a
 void main() {
-  final controller = StackedStreamManager<String>();
+  final controller = LastOnlyEmitter();
 
-  void doSomething(num id, dynamic value){
+  void doSomething(num id, dynamic value) {
     print("From subscription $id");
     print(value);
   }
 
-  late final StreamSubscription subscription1;
-  late final StreamSubscription subscription2;
-  late final StreamSubscription subscription3;
-  late final StreamSubscription subscription4;
+  late final FoundationErrorDisposer subscription1;
+  late final FoundationErrorDisposer subscription2;
+  late final FoundationErrorDisposer subscription3;
+  late final FoundationErrorDisposer subscription4;
 
   subscription1 = controller.listen((value) {
     doSomething(1, value);
-    subscription1.cancel();
+    subscription1.disposeCallback();
   });
   subscription2 = controller.listen((value) {
     doSomething(2, value);
-    subscription2.cancel();
+    subscription2.disposeCallback();
   });
   subscription3 = controller.listen((value) {
     doSomething(3, value);
-    subscription3.cancel();
+    subscription3.disposeCallback();
   });
   subscription4 = controller.listen((value) {
     doSomething(4, value);
-    subscription4.cancel();
+    subscription4.disposeCallback();
   });
 
   Future.delayed(Duration(seconds: 1), () {
     controller.add("Should be from fourth");
   });
 
-  Future.delayed(Duration(seconds: 4), () {
+  Future.delayed(Duration(seconds: 2), () {
     controller.add("Should be from third");
   });
 
-  Future.delayed(Duration(seconds: 6), () {
+  Future.delayed(Duration(seconds: 3), () {
     controller.add("Should be from second");
   });
 
-  Future.delayed(Duration(seconds: 7), () {
+  Future.delayed(Duration(seconds: 4), () {
     controller.add("Shluld be from first");
   });
 }
 
-class StackedStreamController {
+/// A simple event-emitter that only emits to the last listener in the stack.
+class LastOnlyEmitter<T> {
+  final List<Function(T)> _callbacks = [];
 
-}
+  LastOnlyEmitter();
 
-class StackedStreamManager<T> {
-  final StreamController<T> _streamController;
-
-  StackedStreamManager(): _streamController = StreamController();
-
-  List<Function> _listeners = [];
-
-  StreamSubscription listen(Function(T value) callback){
-    _listeners.add(callback);
-
-    _streamController.sink.addStream(stream);
-    _streamController.sink.add(event);
+  add(T value) {
+    _callbacks.last.call(value);
   }
 
-  void add(T event) {
-    _streamController.sink.add(event);
+  FoundationErrorDisposer listen(Function(T) callback) {
+    _callbacks.add(callback);
+
+    // Use the current length of the array as the id.
+    final id = _callbacks.length;
+    return FoundationErrorDisposer(id, () {
+      _callbacks.removeAt(id - 1);
+    });
+  }
+}
+
+class FoundationErrorDisposer {
+  final int id;
+  final Function disposeCallback;
+
+  const FoundationErrorDisposer(this.id, this.disposeCallback);
+
+  disposeListener() {
+    disposeCallback();
   }
 }
