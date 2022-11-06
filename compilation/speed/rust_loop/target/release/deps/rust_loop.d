5b672f8e7a307785fe4079dbf64eb660fc9ad3fe
khongchai <world1955@hotmail.com>
2022-11-06 13:51:21 +0700

    bruh rust is so fast

---
+++ compilation/speed/rust_loop/target/release/deps/rust_loop.d
@@ -0,0 +1,5 @@
+D:\coding notes\general\compilation\speed\rust_loop\target\release\deps\rust_loop.exe: src\main.rs
+
+D:\coding notes\general\compilation\speed\rust_loop\target\release\deps\rust_loop.d: src\main.rs
+
+src\main.rs:
---
+++ compilation/speed/rust_loop/target/release/rust_loop.d
@@ -0,0 +1,1 @@
+D:\coding\ notes\general\compilation\speed\rust_loop\target\release\rust_loop.exe: D:\coding\ notes\general\compilation\speed\rust_loop\src\main.rs
