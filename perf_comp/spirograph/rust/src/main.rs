ce7c9e804bbcd1be81b84ce3da76d2ce8aef0f90
khongchai <k.greesuradej@accenture.com>
2022-12-06 23:09:10 +0700

    fixed a bug

--- perf_comp/spirograph/rust/src/main.rs
+++ perf_comp/spirograph/rust/src/main.rs
@@ -7,11 +7,12 @@ use std::time::Instant;
 fn main() {
     let start = Instant::now();
 
-    let result = calc_lines(1000000, 0.0, 0.0006283185307179586, 100.0);
+    let results = calc_lines(1000000, 0.0, 0.0006283185307179586, 100.0);
 
     let duration = start.elapsed();
 
-    println!("result length: {}", result.len());
+    println!("result length: {}", results.len());
+    println!("Sample results: {}, {}, {}, {}", results[0], results[1], results[99], results[1000]);
     println!("Time elapsed in calc_lines() is: {:?}", duration);
 }
 
@@ -22,22 +23,18 @@ pub fn calc_lines(points: usize, mut theta: f64, step: f64, rod_length: f64) -> 
     let mut prev_point: [f64; 2] = [0.0, 0.0];
     let mut new_point: [f64; 2] = [0.0, 0.0];
 
-    // Use vec because in the actual code (where this came from),
-    // we do not know the size of the outer array at compile time.
-    let parsed_data: *const [f64; 4] = vec![
+    let parsed_data: Vec<[f64; 4]> = vec![
         [300.0, 100.0, 0.0, 1.0],
         [100.0, 50.0, 1.0, 5.11],
         [50.0, 25.0, 1.0, 5.11],
         [25.0, 50.0, 0.0, 7.0],
-    ].as_ptr();
-    let inner_data_size: usize = 4;
-
-    if inner_data_size < 2 {
-        panic!("Provide at least 2 cycloids");
-    }
+    ];
+    let parsed_data_crunched = parsed_data.iter().map(
+        |a| [a[0] + a[1], PI * 0.5 * a[2], a[3]]
+    ).collect();
 
     for _ in 0..points {
-        compute_epitrochoid(parsed_data, theta, rod_length, &mut new_point, inner_data_size);
+        compute_epitrochoid(&parsed_data_crunched, theta, rod_length, &mut new_point);
 
         if first_time {
             first_time = false;
@@ -57,21 +54,20 @@ pub fn calc_lines(points: usize, mut theta: f64, step: f64, rod_length: f64) -> 
 }
 
 pub fn compute_epitrochoid(
-    data: *const [f64; 4],
+    data: &Vec<[f64; 3]>,
     theta: f64,
     rod_length: f64,
     new_point: &mut [f64; 2],
-    inner_data_size: usize,
 ) {
-    unsafe {
-        for i in 0..inner_data_size {
-            let d = data.add(i).as_ref().unwrap();
-            new_point[0] += (d[0] + d[1]) * (theta * d[3] - PI * 0.5 * d[2]).cos();
-            new_point[1] += (d[0] + d[1]) * (theta * d[3] + PI * 0.5 * d[2]).sin();
-        }
-
-        new_point[0] += rod_length * theta.cos();
-        new_point[1] += rod_length * theta.sin();
-
+    if data.len() < 2 {
+        panic!("Provide at least 2 cycloids");
     }
+
+    for d in data {
+        new_point[0] += d[0] * (theta * d[2] - d[1]).cos();
+        new_point[1] += d[0] * (theta * d[2] + d[1]).sin();
+    }
+
+    new_point[0] += rod_length * theta.cos();
+    new_point[1] += rod_length * theta.sin();
 }
