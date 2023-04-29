1168962f4615d67a39521fd597445b9251c9e6b2
khongchai <world1955@hotmail.com>
2023-04-29 19:50:43 +0700

    so basically I got the idea wrong hahaha

--- algo/suffix_tree_I.dart
+++ algo/suffix_tree_I.dart
@@ -21,7 +21,7 @@ class PrefixTree {
   late final PrefixNode rootNode;
 
   PrefixTree({required List<String> words}) {
-    rootNode = PrefixNode(char: "");
+    rootNode = PrefixNode(char: "", parent: null);
     for (final word in words) {
       _buildPrefixSubTree(word, rootNode);
     }
@@ -30,11 +30,13 @@ class PrefixTree {
   // Construct prefix sub tree, returns the first node.
   // Top down
 
-  // TODO correct the construction, it's kind of wrong?
+  // TOOD make iterative.
   PrefixNode _buildPrefixSubTree(String words, PrefixNode parentNode) {
     if (words.isEmpty) return parentNode;
-    final newNode = PrefixNode(char: words[0]);
-    if (!parentNode.hasChild(newNode)) {
+    PrefixNode newNode = PrefixNode(char: words[0], parent: parentNode);
+    if (parentNode.hasChild(newNode)) {
+      newNode = parentNode.getChild(newNode);
+    } else {
       parentNode.addChild(newNode);
     }
     return _buildPrefixSubTree(words.substring(1), newNode);
@@ -44,25 +46,38 @@ class PrefixTree {
 // Returned string should be something like "['c', 'ca', 'cat', 'co']"
   @override
   String toString() {
-    late PrefixNode node = rootNode;
-
-    String returnWord(PrefixNode node) {
-      if (node.children.isEmpty) {
-        return node.char + ",";
+    // TODO make iterative.
+    String writeWord(PrefixNode currentNode, String stringSoFar) {
+      final newString = stringSoFar + currentNode.char;
+      if (currentNode.children.isEmpty) {
+        return newString + ", ";
       }
-      return node.char +
-          node.children.map((element) => returnWord(element)).join("");
+
+      return currentNode.children
+          .map((childNode) => writeWord(childNode, newString))
+          .join();
     }
 
-    return "[${returnWord(node)}]";
+    return '[${writeWord(rootNode, "")}]';
   }
+
+  // List<String> writeWord(PrefixNode currentNode, String stringSoFar) {
+  //   final newString = stringSoFar + currentNode.char;
+  //   if (currentNode.children.isEmpty) {
+  //     return newString + ", ";
+  //   }
+
+  //   return currentNode.children.map((childNode) => writeWord(childNode, newString)).toList();
+  // }
 }
 
 class PrefixNode {
   final String char;
   final Set<PrefixNode> children = {};
+  final PrefixNode? parent;
 
-  PrefixNode({required this.char}) : assert(char.length <= 1);
+  PrefixNode({required this.char, required this.parent})
+      : assert(char.length <= 1);
 
   void addChild(PrefixNode child) {
     children.add(child);
@@ -70,6 +85,12 @@ class PrefixNode {
 
   bool hasChild(PrefixNode prefixNode) {
     return children.contains(prefixNode);
+  }
+
+  PrefixNode getChild(PrefixNode prefixNode) {
+    final child = children.lookup(prefixNode);
+    assert(child != null);
+    return child!;
   }
 
   @override
@@ -82,10 +103,27 @@ class PrefixNode {
   int get hashCode {
     return char.hashCode;
   }
+
+  @override
+  String toString() {
+    return char;
+  }
 }
 
+// Problem: If a word is a subsequence of another word in the array, it'll just get ignored. Actually, this is because the entire thing is wrong...
 void main() {
-  const words = ["cat", "co", "dog", "dot"];
+  // const words = ["cat", "co", "dog", "dot"]
+  const words = [
+    "chicken",
+    "chic",
+    "cow",
+    "ants",
+    "a",
+    "aa",
+    "aaa",
+    "chip",
+    "dog"
+  ];
   const Map<Question, Answer> testCases = {
     "": ["cat", "ca", "c", "co", "d", "do", "dog", "dot"],
     "ca": ["t"],
@@ -95,6 +133,7 @@ void main() {
   };
   final trie = PrefixTree(words: words);
   print(trie);
+
   // testCases.entries.forEach((element) {
   //   final answer = trie.getAllWords(element.key);
   //   assert(
